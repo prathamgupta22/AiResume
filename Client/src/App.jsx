2db2db3a47f6e0557d3e7e151b5b67493910e511
Pratham22102001 <pratham.guptapg22@gmail.com>
2024-08-04 16:54:56 +0530

    project started

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+const App = () => {
+  return (
+    <>
+      <div>
+        App
+        <Button>Subscribe</Button>
+      </div>
+    </>
+  );
+};
+
+export default App;
